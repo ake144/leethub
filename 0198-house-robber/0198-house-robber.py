e8c4c312fc041ddb35cce10be967c5154390a010
Aklilu Tamirat <122739445+ake144@users.noreply.github.com>
2023-11-08 14:21:18 -0800

    Time: 39 ms (54.84%), Space: 16.2 MB (44.21%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        even_sum = 0
+        odd_sum = 0
+
+        for i in range(len(nums)):
+            if i % 2 == 0:
+                even_sum = max(even_sum + nums[i], odd_sum)
+            else:
+                odd_sum = max(odd_sum + nums[i], even_sum)
+
+        return max(even_sum, odd_sum)     
+    
